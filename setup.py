d09fd39a3bebac595f78f70fc3962354b43c74d9
Wilka Carvalho <wcarvalho92@gmail.com>
2024-07-01 12:02:56 -0400

    setup + reqs

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WebRL",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+        "flask-socketio",
+        #"python-dotenv",
+        "Flask-Cors",
+        #"gunicorn"
+        #"gevent"
+    ],
+    author="Wilka Carvalho",
+    author_email="wcarvalho92@gmail.com",
+    description="A library to help create flask-based python web app for reinforcement learning experiments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wcarvalho/webrl",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
